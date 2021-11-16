b0902440ab6840147c8ddb0388c53e71de4ab99a
Matthew Tralka <mtralka59@gmail.com>
2021-11-16 10:25:26 -0500

    :lipstick: Update mobile ColorKey styles

--- mgwrvisualizer/src/components/Map/ColorKey.vue
+++ mgwrvisualizer/src/components/Map/ColorKey.vue
@@ -19,30 +19,30 @@ const getColor = (index) => {
 
 <template>
 
-<div class="min-w-48 w-auto bg-white z-50 w-40 flex justify-center flex-col ">
-    <div v-if="selectionStore.getVizualizationType == VizType.ATTRIBUTE && selectionStore.getSelectedAttribute != null" class="py-2">
-        <div v-for="(bucket, index) in selectionStore.getCategoricalBuckets" :key="index"  class="flex flex-row justify-evenly w-full" > 
-            <div class="flex-shrink-0 w-[4.5rem] flex-grow font-mono text-xs text-black px-2 leading-none tracking-tighter flex justify-center items-center">
+<div class="min-w-30 md:min-w-48 bg-white z-50 w-40 flex justify-center flex-col ">
+    <div v-if="selectionStore.getVizualizationType == VizType.ATTRIBUTE && selectionStore.getSelectedAttribute != null" class="py-1 md:py-2">
+        <div v-for="(bucket, index) in selectionStore.getCategoricalBuckets" :key="index"  class="key" > 
+            <div class="text">
                 {{selectionStore.getCategoricalBuckets[index - 1] ? selectionStore.getCategoricalBuckets[index - 1].toFixed(2).toString() : "min"}}
                 -
                 {{bucket.toFixed(2).toString()}}
             </div>
             <div
-            class=" flex-grow  min-h-8 flex-grow-[3]"
+            class="color"
             :style="{'background-color' : getColor(index)}"
             >
             </div>
         </div>
     </div>
-    <div v-if="selectionStore.getVizualizationType == VizType.BANDWITH" class="py-2">
-        <div v-for="(number, index) in bandwithRange" :key="index"  class="flex flex-row justify-evenly w-full" > 
-            <div class="flex-shrink-0 w-[4.5rem] flex-grow font-mono text-xs text-black px-2 leading-none tracking-tighter flex justify-center items-center">
+    <div v-if="selectionStore.getVizualizationType == VizType.BANDWITH" class="py-1 md:py-2">
+        <div v-for="(number, index) in bandwithRange" :key="index"  class="key" > 
+            <div class="text">
                 {{bandwithRange[index] ? bandwithRange[index - 1]  : "0"}}
                 -
                 {{number }}
             </div>
             <div
-            class=" flex-grow  min-h-8 flex-grow-[3]"
+            class="color"
             :style="{'background-color' : d3scaleChromatic.interpolateReds(number)}"
             >
             </div>
@@ -56,5 +56,17 @@ const getColor = (index) => {
 <style>
 select {
     background-color: (233,299,321);
+}
+
+.key {
+    @apply flex flex-row justify-evenly w-full;
+}
+
+.key .text {
+    @apply flex-shrink-0 w-[4.5rem] flex-grow font-mono text-xs text-black px-1 md:px-2 leading-none tracking-tighter flex justify-center items-center;
+}
+
+.key .color {
+    @apply flex-grow min-h-8 flex-grow-[3];
 }
 </style>
