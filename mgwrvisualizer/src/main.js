31aa35e7893e69706214f028c31b4e777fe9b54c
mtralka <mtralka@umd.edu>
2021-11-16 21:53:30 -0500

    :bug: In router pathfinding

--- mgwrvisualizer/src/main.js
+++ mgwrvisualizer/src/main.js
@@ -1,7 +1,7 @@
-import router from '@/router'
 import { createPinia } from 'pinia'
 import 'virtual:windi.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 createApp(App).use(router).use(createPinia()).mount('#app')
